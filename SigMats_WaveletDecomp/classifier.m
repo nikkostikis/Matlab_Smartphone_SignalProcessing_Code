6249f4fe2f34aa423b3b3399eac7de51819dcd34
Nick Kostikis <nikkostikis@gmail.com>
2016-03-29 01:22:49 +0300

    Add support for full training set

--- SigMats_WaveletDecomp/classifier.m
+++ SigMats_WaveletDecomp/classifier.m
@@ -5,12 +5,6 @@ clear all;
 % clc;
 close all;
 
-OH = 20;
-PD =26;
-sizetrain = 12;
-pOH = OH - sizetrain;
-pPD = PD - sizetrain;
-
 % load wavDetCoefsPower_All which is 46x32:
 % 20/OH + 26/PD x (4/HM x 8 DWT levels)
 % HM goes like: EL/ER/RL/RR
@@ -19,46 +13,78 @@ createTags_HM_Level;
 
 [m n] = size(wavDetCoefsPower_All);
 
-% random indices to remove for prediction
+OH = 20;
+PD =26;
+% if sizetrain == OH||PD all for train and all for prediction
+sizetrainOH = OH
+sizetrainPD = PD
+pOH = OH - sizetrainOH;
+pPD = PD - sizetrainPD;
+pOH = OH;
+pPD = PD;
+
+
+% random indices to remove for training & prediction
 dataOH = wavDetCoefsPower_All(1:OH, :);
 randpermOH = randperm(OH);
-trainIndOH = randpermOH(1:sizetrain);
-predIndOH = randpermOH(sizetrain+1:end);
+trainIndOH = randpermOH(1:sizetrainOH);
 % slice
 trainOH = dataOH(trainIndOH, :);
-predOH = dataOH(predIndOH, :);
 
 dataPD = wavDetCoefsPower_All(OH+1:end, :);
 randpermPD = randperm(PD);
-trainIndPD = randpermPD(1:sizetrain);
-predIndPD = randpermPD(sizetrain+1:end);
+trainIndPD = randpermPD(1:sizetrainPD);
 % slice
 trainPD = dataPD(trainIndPD, :);
-predPD = dataPD(predIndPD, :);
 
+% setup training array
 train = [trainOH; trainPD];
-ratingtrain = [zeros(sizetrain,1) ; ones(sizetrain,1)];
+ratingtrain = [zeros(sizetrainOH,1) ; ones(sizetrainPD,1)];
 rating_celltrain=num2str(ratingtrain);
 rating_celltrain=cellstr(rating_celltrain);
 
-pred = [predOH; predPD];
-ratingpred = [zeros(pOH,1) ; ones(pPD,1)];
-rating_cellpred=num2str(ratingpred);
-rating_cellpred=cellstr(rating_cellpred);
 
+% setup prediction array
+if (sizetrainOH~=OH && sizetrainPD~=PD)
+    predIndOH = randpermOH(sizetrainOH+1:end);
+    predOH = dataOH(predIndOH, :);
+    predIndPD = randpermPD(sizetrainPD+1:end);
+    predPD = dataPD(predIndPD, :);
+    pred = [predOH; predPD];
+    ratingpred = [zeros(pOH,1) ; ones(pPD,1)];
+    rating_cellpred=num2str(ratingpred);
+    rating_cellpred=cellstr(rating_cellpred);
+elseif ( (sizetrainOH~=OH && sizetrainPD==PD) || ...
+        (sizetrainOH==OH && sizetrainPD~=PD) )
+    disp('Each class must have training and prediction instances');
+else
+    pred = train;
+    ratingpred = [zeros(sizetrainOH,1) ; ones(sizetrainPD,1)];
+    rating_cellpred=num2str(ratingpred);
+    rating_cellpred=cellstr(rating_cellpred);
+end
 % keyboard;
 
 
-nTrees = 700;
+nTrees = 1000
 
 
 % b = TreeBagger(nTrees, features, rating, 'oobvarimp', 'on', 'oobpred', 'on');
 b = TreeBagger(nTrees, train, ratingtrain, 'Method' , 'classification', 'oobpred', 'on', 'oobvarimp', 'on');
-[Yfit, Sfit] = b.predict(pred);
-[conf classorder] = confusionmat(rating_cellpred, Yfit);
+% [Yfit, Sfit] = b.predict(pred);
+% [conf classorder] = confusionmat(rating_cellpred, Yfit);
+% disp(dataset({conf, classorder{:}}, 'obsnames', classorder));
+[Yfit, Sfit] = oobPredict(b);
+[conf classorder] = confusionmat(rating_celltrain, Yfit);
 disp(dataset({conf, classorder{:}}, 'obsnames', classorder));
+[XX,YY,TT,AUC,optrocpt]=perfcurve(rating_celltrain, Sfit(:,2), classorder{2});
+AUC
+
+
 TpTn = diag(conf);
 disp({'TN:', num2str(TpTn(1)/pOH), 'TP:', num2str(TpTn(2)/pPD)});
+
+
 
 figure(100)
 plot(oobError(b));
@@ -70,7 +96,7 @@ hold on;
 % bar(abs(b.OOBPermutedVarDeltaError));
 OOBPVDE = b.OOBPermutedVarDeltaError;
 positiveFeatures = find(OOBPVDE >= 0);
-negativeFeatures = find(OOBPVDE < 0);
+negativeFeatures = find(OOBPVDE < 0)
 % plot positive blue
 OOBPVDE_pos = OOBPVDE;
 OOBPVDE_pos(negativeFeatures) = 0;
